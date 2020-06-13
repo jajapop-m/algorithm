b6c1ad1fc3b0506dff430d481ffa39d4fe641102
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-06-13 12:26:58 +0900

    bubbleSort

--- 2_bubbleSort.rb
+++ 2_bubbleSort.rb
@@ -7,12 +7,13 @@
 #   8（交換回数）
 
 def bubbleSort(a, n)
-  flag = true, count = 0
+  flag = true
+  count = 0
   while flag do
     for i in 0..n-1 do
       flag = false
-      for j in n-1..i+1 do
-        if a[j-1] > a[j]
+      for j in (i+1..n-1).to_a.reverse do
+        if a[j] < a[j-1]
           a[j-1], a[j] = a[j], a[j-1]
           flag = true
           count += 1
@@ -20,15 +21,11 @@ def bubbleSort(a, n)
       end
     end
   end
-  trace(a, count)
-end
-
-def trace(a, count)
   puts a.join(" ")
   puts count
 end
 
+
 n = gets.to_i
 a = gets.split.map(&:to_i)
-
 bubbleSort(a, n)

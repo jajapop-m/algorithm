a1460f9b1741961f5969a1d95797c9a535f917ae
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-07-15 23:07:08 +0900

    cleanLS

--- DPL_3_A_LargestSquare.rb
+++ DPL_3_A_LargestSquare.rb
@@ -22,22 +22,15 @@ Clean = 0
 def max_square(lines)
   dp = Array.new(lines.length){Array.new}
   max_width = 0
-  for i in 0..lines.length-1
-    for j in 0..lines[i].length-1
-      if i == 0 || j == 0
-        if lines[i][j] == Clean
-          dp[i][j] = 1
-        elsif lines[i][j] == Dirty
-          dp[i][j] = 0
-        end
-        next
-      end
-      next dp[i][j] = 0 if lines[i][j] == Dirty
+  lines.each_with_index do |line, i|
+    line.each_with_index do |status, j|
+      next dp[i][j] = 0 if status == Dirty
+      next dp[i][j] = 1 if i == 0 || j == 0 && status == Clean
       dp[i][j] = [dp[i][j-1], dp[i-1][j], dp[i-1][j-1]].min + 1
       max_width = [max_width, dp[i][j]].max
     end
   end
-  [max_width**2, dp]
+  max_width**2
 end
 
-p max_square(lines)
+puts max_square(lines)

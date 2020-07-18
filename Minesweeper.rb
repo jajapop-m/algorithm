ea665f9a40427df7bc79f1d49b26c019b6d1c24d
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-07-19 01:30:51 +0900

    hotfix_and_clean_Minesweeper

--- Minesweeper.rb
+++ Minesweeper.rb
@@ -33,83 +33,16 @@ class Board
     check_game_status
   end
 
-  def reveal_loop(i,j)
-    lists[i][j].revealed = true
-    return if lists[i][j].status == :bomb
-    return if (i < 0 || i > n-1 || j < 0 || j > n-1) || \
-              lists[i][j].status != 0 && lists[i][j].status.to_s.match(/\d/)
-    reveal_loop(i-1,j-1) if within_range?(i-1,j-1) && (not lists[i-1][j].revealed?)
-    reveal_loop(i-1,j)   if within_range?(i-1,j)   && (not lists[i-1][j].revealed?)
-    reveal_loop(i-1,j+1) if within_range?(i-1,j+1) && (not lists[i-1][j+1].revealed?)
-    reveal_loop(i+1,j-1) if within_range?(i+1,j-1) && (not lists[i+1][j-1].revealed?)
-    reveal_loop(i+1,j)   if within_range?(i+1,j)   && (not lists[i+1][j].revealed?)
-    reveal_loop(i+1,j+1) if within_range?(i+1,j+1) && (not lists[i+1][j+1].revealed?)
-    reveal_loop(i,j-1)   if within_range?(i,j-1)   && (not lists[i][j-1].revealed?)
-    reveal_loop(i,j+1)   if within_range?(i,j+1)   && (not lists[i][j+1].revealed?)
-  end
-
-  def game_over
-    lists.flatten.each do |l|
-      l.revealed = true if l.status == :bomb
-    end
-    puts_list
-    puts "GAME OVER".center(n*(n.to_s.length)+n.to_s.length)
-  end
-
-  def game_over?
-    lists.flatten.each do |l|
-      return true if l.status == :bomb && l.revealed == true
-    end
-    false
-  end
-
   def check_game_status
-    if game_over?
-      game_over
-      return
-    elsif game_clear?
-      puts_list
-      puts "CLEAR!!".center(n*(n.to_s.length)+n.to_s.length)
-      return
-    end
+    return game_over  if game_over?
+    return game_clear if game_clear?
     puts_list
-  end
-
-  def game_clear?
-    count = 0
-    lists.flatten.each do |l|
-      count += 1 if l.revealed?
-    end
-    count == n*n - bomb_count
   end
 
   def puts_list
-    result = []
-    lists.flatten.each do |l|
-      if l.revealed
-        case s =  l.status
-        when 0
-          result << "□"
-        when :bomb
-          result << "B"
-        else
-          result << s
-        end
-      else
-        result << "■"
-      end
-    end
-    result.unshift([*1..n]).flatten!
-    res = []
-    result.each_slice(n).with_index do |r,i|
-      next res[i] = r.unshift(" ") if i == 0
-      res[i] = r.unshift(i)
-    end
-    len = [n.to_s.length, 2].max
-    res.each do |r|
-      r.each do |a|
-          print a.to_s.rjust(len)
-      end
+    cur_stat_with_line_numbers = current_statuses
+    cur_stat_with_line_numbers.each do |r|
+      r.each {|a| print a.to_s.rjust([n.to_s.length, 2].max)}
       puts "\r"
     end
   end
@@ -127,28 +60,101 @@ class Board
     numbers_set
   end
 
+  def continuing?
+    !game_over? && !game_clear?
+  end
+
   private
 
-  def numbers_set
-    bomb_map.each_slice(n).with_index do |line, i|
-      line.each_with_index do |status, j|
-        if status
-          lists[i-1][j-1].status += 1 if (i != 0 && j != 0)     && lists[i-1][j-1].status != :bomb
-          lists[i-1][j].status += 1   if i != 0                 && lists[i-1][j].status != :bomb
-          lists[i-1][j+1].status += 1 if (i != 0 && j != n-1)   && lists[i-1][j+1].status != :bomb
-          lists[i][j-1].status += 1   if j != 0                 && lists[i][j-1].status != :bomb
-          lists[i][j+1].status += 1   if j != n-1               && lists[i][j+1].status != :bomb
-          lists[i+1][j-1].status += 1 if (i != n-1 && j != 0)   && lists[i+1][j-1].status != :bomb
-          lists[i+1][j].status += 1   if i != n-1               && lists[i+1][j].status != :bomb
-          lists[i+1][j+1].status += 1 if (i != n-1 && j != n-1) && lists[i+1][j+1].status != :bomb
+    def reveal_loop(i,j)
+      lists[i][j].revealed = true
+      return if lists[i][j].status == :bomb
+      return if (i < 0 || i > n-1 || j < 0 || j > n-1) || \
+                lists[i][j].status != 0 && lists[i][j].status.to_s.match(/\d/)
+      reveal_loop(i-1,j-1) if within_range?(i-1,j-1) && (not lists[i-1][j].revealed?)
+      reveal_loop(i-1,j)   if within_range?(i-1,j)   && (not lists[i-1][j].revealed?)
+      reveal_loop(i-1,j+1) if within_range?(i-1,j+1) && (not lists[i-1][j+1].revealed?)
+      reveal_loop(i+1,j-1) if within_range?(i+1,j-1) && (not lists[i+1][j-1].revealed?)
+      reveal_loop(i+1,j)   if within_range?(i+1,j)   && (not lists[i+1][j].revealed?)
+      reveal_loop(i+1,j+1) if within_range?(i+1,j+1) && (not lists[i+1][j+1].revealed?)
+      reveal_loop(i,j-1)   if within_range?(i,j-1)   && (not lists[i][j-1].revealed?)
+      reveal_loop(i,j+1)   if within_range?(i,j+1)   && (not lists[i][j+1].revealed?)
+    end
+
+    def numbers_set
+      bomb_map.each_slice(n).with_index do |line, i|
+        line.each_with_index do |status, j|
+          if status
+            lists[i-1][j-1].status += 1 if (i != 0 && j != 0)     && lists[i-1][j-1].status != :bomb
+            lists[i-1][j].status += 1   if i != 0                 && lists[i-1][j].status != :bomb
+            lists[i-1][j+1].status += 1 if (i != 0 && j != n-1)   && lists[i-1][j+1].status != :bomb
+            lists[i][j-1].status += 1   if j != 0                 && lists[i][j-1].status != :bomb
+            lists[i][j+1].status += 1   if j != n-1               && lists[i][j+1].status != :bomb
+            lists[i+1][j-1].status += 1 if (i != n-1 && j != 0)   && lists[i+1][j-1].status != :bomb
+            lists[i+1][j].status += 1   if i != n-1               && lists[i+1][j].status != :bomb
+            lists[i+1][j+1].status += 1 if (i != n-1 && j != n-1) && lists[i+1][j+1].status != :bomb
+          end
         end
       end
     end
-  end
 
-  def within_range?(i,j)
-    i >= 0 && i < n && j >= 0 && j < n
-  end
+    def game_over
+      lists.flatten.each do |l|
+        l.revealed = true if l.status == :bomb
+      end
+      puts_list
+      puts "GAME OVER".center(n*(n.to_s.length)+n.to_s.length)
+    end
+
+    def game_over?
+      lists.flatten.each do |l|
+        return true if l.status == :bomb && l.revealed == true
+      end
+      false
+    end
+
+    def game_clear
+      puts_list
+      puts "CLEAR!!".center(n*(n.to_s.length)+n.to_s.length)
+    end
+
+    def game_clear?
+      count = 0
+      lists.flatten.each do |l|
+        count += 1 if l.revealed?
+      end
+      count == n*n - bomb_count
+    end
+
+    def within_range?(i,j)
+      i >= 0 && i < n && j >= 0 && j < n
+    end
+
+    def current_statuses
+      cur_stat = []
+      lists.flatten.each do |l|
+        next  cur_stat << "■" unless l.revealed
+        case s =  l.status
+        when 0
+          cur_stat << "□"
+        when :bomb
+          cur_stat << "B"
+        else
+          cur_stat << s
+        end
+      end
+      add_line_numbers(cur_stat)
+    end
+
+    def add_line_numbers(cur_stat)
+      cur_stat.unshift([*1..n]).flatten!
+      cur_stat_with_line_numbers = []
+      cur_stat.each_slice(n).with_index do |r,i|
+        next cur_stat_with_line_numbers[i] = r.unshift(" ") if i == 0
+        cur_stat_with_line_numbers[i] = r.unshift(i)
+      end
+      cur_stat_with_line_numbers
+    end
 end
 
 class Minesweeper
@@ -178,7 +184,7 @@ class Minesweeper
   end
 
   def try_game
-    while not (board.game_clear? || board.game_over?)
+    while board.continuing?
       print "縦 横:"
       i,j = gets.split.map(&:to_i)
       if (not i) || (not j) || (i < 0 || i > n) || (j < 0 || j > n)
